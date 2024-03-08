7f01c8edf126113bc97dc81c39ab172c7ac65ee4
YanagiOrigami <82631512+YanagiOrigami@users.noreply.github.com>
2024-03-07 23:32:25 -0800

    target

--- ToxiCN_ex/train_eval.py
+++ ToxiCN_ex/train_eval.py
@@ -56,9 +56,9 @@ def train(config, train_iter, dev_iter, test_iter, task=1):
             # label = args['expression']
             label = args['target']
             loss = loss_fn(logit, label.float())
-            pred = get_preds(config, logit)  
+            # pred = get_preds(config, logit)  
             # pred = get_preds_task2_4(config, logit)  
-            # pred = get_preds_task3(config, logit)  
+            pred = get_preds_task3(config, logit)  
             preds.extend(pred)
             labels.extend(label.detach().numpy())
 
@@ -122,9 +122,9 @@ def eval(config, embed_model, model, loss_fn, dev_iter, data_name='DEV'):
             # label = args['expression']
             label = args['target']
             loss = loss_fn(logit, label.float())
-            pred = get_preds(config, logit)  
+            #pred = get_preds(config, logit)  
             #pred = get_preds_task2_4(config, logit)  
-            #pred = get_preds_task3(config, logit)  
+            pred = get_preds_task3(config, logit)  
             preds.extend(pred)
             labels.extend(label.detach().numpy())
             loss_all += loss.item()
