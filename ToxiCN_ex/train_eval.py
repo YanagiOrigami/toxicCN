bb23c6d8b9c885273a91ca684c74319c8db047f1
YanagiOrigami <82631512+YanagiOrigami@users.noreply.github.com>
2024-03-07 21:27:27 -0800

    type

--- ToxiCN_ex/train_eval.py
+++ ToxiCN_ex/train_eval.py
@@ -52,13 +52,13 @@ def train(config, train_iter, dev_iter, test_iter, task=1):
             logit = model(att_input, pooled_emb).cpu()
 
             # label = args['toxic']
-            # label = args['toxic_type']
-            label = args['expression']
+            label = args['toxic_type']
+            # label = args['expression']
             # label = args['target']
             loss = loss_fn(logit, label.float())
             # pred = get_preds(config, logit)  
-            # pred = get_preds_task2_4(config, logit)  
-            pred = get_preds_task3(config, logit)  
+            pred = get_preds_task2_4(config, logit)  
+            # pred = get_preds_task3(config, logit)  
             preds.extend(pred)
             labels.extend(label.detach().numpy())
 
